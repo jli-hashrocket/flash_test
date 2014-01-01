c9e2a7db163582388cf0870a97c6f979c96bd5ce
macheteshuffle <jeff.li029@gmail.com>
2014-01-01 15:18:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashTest::Application.config.secret_key_base = '3279d2e8e18063be0b75f6c082843f3f2d474193748331b095a98f94af7dbb9e4d799ef761534a6999da2912e158b8cc533de7b694cc01d36f030537a18b086d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlashTest::Application.config.session_store :cookie_store, key: '_flash_test_session'
